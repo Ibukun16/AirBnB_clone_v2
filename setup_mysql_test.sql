1386ae80c642996573687abbc1be205bbc590d61
Ibukun16 <ib.agunbiade18@gmail.com>
2024-07-01 22:49:22 -0700

    update test MySQL server

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,8 +4,8 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db
 -- creating a new user named : hbnb_test, in localhost and set password to it.
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED WITH mysql_native_password BY 'hbnb_test_pwd';
 -- grant all privilege for user 'hbnb_test' on the database hbnb_test_db
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
 -- grant privilege of SELECT to user 'hbnb_test' on the database performance_schema
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
