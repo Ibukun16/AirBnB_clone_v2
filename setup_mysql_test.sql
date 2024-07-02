53935be6134dd32fceda954f3236ec0a3076b3e6
Ibukun16 <ib.agunbiade18@gmail.com>
2024-07-01 22:51:15 -0700

    update test MySQL server

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
 -- A script that prepares a MySQL server for this project
 -- creating a project testing database with the name : hbnb_test_db
-CREATE DATABASE IF NOT EXISTS hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- creating a new user named : hbnb_test, in localhost and set password to it.
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED WITH mysql_native_password BY 'hbnb_test_pwd';
 -- grant all privilege for user 'hbnb_test' on the database hbnb_test_db
