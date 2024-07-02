63d334d3ea189f03881cdb397d1137bbaf462e0d
Ibukun16 <ib.agunbiade18@gmail.com>
2024-07-01 22:45:45 -0700

    update MySQL server

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,8 +5,8 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- password for 'hbnh_dev_db' set to hbnb_dev_pwd.
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED WITH mysql_native_password BY 'hbnb_dev_pwd';
 -- grant all privileges to new user 'hbnb_dev'
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'
+GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
 -- grant SELECT privilege for the user hbnb_dev in the db performance_schema
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
