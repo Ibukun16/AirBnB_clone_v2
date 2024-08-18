03fd9a97eb279a9119eed95822fbcafbe11947cb
Ibukun16 <ib.agunbiade18@gmail.com>
2024-08-18 01:17:47 -0700

    AirBnB web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,6 +13,22 @@ env.user = "ubuntu"
 env.key_filename = '~/.ssh/school'
 
 
+def do_pack():
+    """
+        return the archive path if archive has generated correctly.
+    """
+
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archived_f_path = "versions/web_static_{}.tgz".format(date)
+    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
+
+    if t_gzip_archive.succeeded:
+        return archived_f_path
+    else:
+        return None
+
+
 def do_deploy(archive_path):
     """Deploy web files to server."""
     try:
@@ -29,7 +45,7 @@ def do_deploy(archive_path):
 
         # uncompress archive and delete .tgz
         run(f"sudo tar -xzf /tmp/web_static_{timestamp}.tgz -C \
-/data/web_static/releases/web_static_{timestamp}/")
+    /data/web_static/releases/web_static_{timestamp}/")
 
         # remove archive
         run(f'sudo rm /tmp/web_static_{timestamp}.tgz')
@@ -46,8 +62,8 @@ def do_deploy(archive_path):
         run('sudo rm -rf /data/web_static/current')
 
         # re-establish symbolic link
-        run(f'sudo ln -s /data/web_static/releases/\
-            web_static_{timestamp}/ /data/web_static/current')
+        run(f'sudo ln -s /data/web_static/releases/web_static_{timestamp}/ \
+                /data/web_static/current')
     except Exception:
         return False
 
