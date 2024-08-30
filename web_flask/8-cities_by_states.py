b16701e81e0a5fb86e869abff5baf7a5c7f347a9
Ibukun16 <ib.agunbiade18@gmail.com>
2024-08-30 05:19:03 -0700

    updating AirBnB 2

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -10,18 +10,18 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.teardow_appcontext
+@app.teardown_appcontext
 def close(self):
     """Close and Clean up session"""
     storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def list_cities_by_states():
+def cities_by_states():
     """Return an HTML page with states and cities"""
     states = storage.all(State)
     return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port="5000")
